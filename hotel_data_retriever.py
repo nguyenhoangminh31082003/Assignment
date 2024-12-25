3cb9aecb056daff9171b59916f1ad97ec57ca33d
Nguyễn Hoàng Minh <95891385+nguyenhoangminh31082003@users.noreply.github.com>
2024-12-25 16:11:27 +0700

    Handle the case when no hotel_id or destination_id is not provided in the input

--- hotel_data_retriever.py
+++ hotel_data_retriever.py
@@ -1,10 +1,9 @@
+from typing import Optional
 from dataclasses import asdict
 from hotel_merger import HotelMerger
 from suppliers.acme_supplier import AcmeSupplier
 from suppliers.patagonia_supplier import PatagoniaSupplier
 from suppliers.paperflies_supplier import PaperfliesSupplier
-
-import json
 
 class HotelDataRetriever:
 
@@ -31,9 +30,20 @@ class HotelDataRetriever:
 
         return [asdict(hotel) for hotel in hotels.values()] 
 
-    def get_hotels(self, hotel_ids: list[str], destination_ids: list[int]) -> list[dict]:
+    def get_hotels(self, hotel_ids: Optional[list[str]], destination_ids: Optional[list[int]]) -> list[dict]:
+
+        if (hotel_ids is None) and (destination_ids is None):
+            return [hotel for hotel in self.hotels]
+        
+        if hotel_ids is None:
+            destination_ids = set(destination_ids)
+            return [hotel for hotel in self.hotels if hotel["destination_id"] in destination_ids]
+        
+        if destination_ids is None:
+            hotel_ids = set(hotel_ids)
+            return [hotel for hotel in self.hotels if hotel["id"] in hotel_ids]
 
         hotel_ids = set(hotel_ids)
         destination_ids = set(destination_ids)
 
-        return [hotel for hotel in self.hotels if hotel['id'] in hotel_ids and hotel['destination_id'] in destination_ids]
+        return [hotel for hotel in self.hotels if hotel["id"] in hotel_ids and hotel["destination_id"] in destination_ids]
--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import json
 from argparse import ArgumentParser
 from hotel_data_retriever import HotelDataRetriever
-from suppliers.acme_supplier import AcmeSupplier
-
 
 def main():
 
     parser = ArgumentParser()
 
-    parser.add_argument("hotel_ids", type=str, help="Hotel IDs")
-    parser.add_argument("destination_ids", type=str, help="Destination IDs")
+    parser.add_argument("hotel_ids", type=str, nargs="?", default = None, help="Hotel IDs")
+    parser.add_argument("destination_ids", type=str, nargs="?", default = None, help="Destination IDs")
 
     arguments = parser.parse_args()
 
-    hotel_ids = arguments.hotel_ids.split(",")
-    destination_ids = list(map(int, arguments.destination_ids.split(",")))
+    hotel_ids = arguments.hotel_ids
+    destination_ids = arguments.destination_ids
+
+    if hotel_ids is not None:
+        hotel_ids = hotel_ids.split(",")
+
+    if destination_ids is not None:
+        destination_ids = list(map(int, destination_ids.split(",")))
 
     result = HotelDataRetriever().get_hotels(hotel_ids, destination_ids)
 
